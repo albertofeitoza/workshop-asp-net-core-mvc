c901048565149f1f7f68179aac5f0a370ace7c7b
Alberto Feitoza <alberto.feitoza@outlook.com>
2019-08-07 07:29:00 -0300

    Populando a Base de dados

--- SalesWebMvc/Data/SeedingService.cs
+++ SalesWebMvc/Data/SeedingService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using SalesWebMvc.Models;
+using SalesWebMvc.Models.Enums;
+using SalesWebMvc.Models.ViewModels;
 
 namespace SalesWebMvc.Data
 {
@@ -10,7 +12,7 @@ namespace SalesWebMvc.Data
     {
         private SalesWebMvcContext _context;
 
-        public SeedingService(SalesWebMvc context)
+        public SeedingService(SalesWebMvcContext context)
         {
             _context = context;
 
@@ -19,7 +21,37 @@ namespace SalesWebMvc.Data
         {
             //testar se existe dados na tabela
 
-            if (_context.Departament.Any());
+            if (_context.Departament.Any()
+                || _context.Seller.Any()
+                || _context.SallesRecord.Any())
+
+            {
+                return; // A base de dados Já foi populada
+            }
+            Departament d1 = new Departament(1, "Computers","12132321","Rua","00000","213131","Alberto");
+            Departament d2 = new Departament(2, "Electronics", "12132321", "Rua", "00000", "213131", "Alberto");
+            Departament d3 = new Departament(3, "Fasshion", "12132321", "Rua", "00000", "213131", "Alberto");
+            Departament d4 = new Departament(4, "Books", "12132321", "Rua", "00000", "213131", "Alberto");
+
+            Seller s1 = new Seller(1, "Vendedor Sistema", "teste@gmail.com", new DateTime(1998, 4, 21), 1000.0, d1);
+            Seller s2 = new Seller(2, "Vendedor Sistema1", "teste@gmail.com", new DateTime(1998, 4, 21), 1000.0, d2);
+            Seller s3 = new Seller(3, "Vendedor Sistema2", "teste@gmail.com", new DateTime(1998, 4, 21), 1000.0, d3);
+            Seller s4 = new Seller(4, "Vendedor Sistema3", "teste@gmail.com", new DateTime(1998, 4, 21), 1000.0, d4);
+
+            SallesRecord r1 = new SallesRecord(1, new DateTime(1998, 1, 21), 11000.0, SaleStatus.Billed, s1);
+            SallesRecord r2 = new SallesRecord(2, new DateTime(1998, 1, 20), 11000.0, SaleStatus.Billed, s2);
+            SallesRecord r3 = new SallesRecord(3, new DateTime(1998, 1, 22), 11000.0, SaleStatus.Billed, s3);
+            SallesRecord r4 = new SallesRecord(4, new DateTime(1998, 1, 27), 11000.0, SaleStatus.Billed, s4);
+            SallesRecord r5 = new SallesRecord(2, new DateTime(1998, 1, 21), 11000.0, SaleStatus.Billed, s1);
+            SallesRecord r6 = new SallesRecord(2, new DateTime(1998, 1, 21), 11000.0, SaleStatus.Billed, s3);
+
+            _context.Departament.AddRange(d1,d2,d3,d4);
+
+            _context.Seller.AddRange(s1, s2 ,s3 ,s4);
+
+            _context.SallesRecord.AddRange(r1, r2, r3, r4, r5, r6);
+
+            _context.SaveChanges();
 
         }
 
