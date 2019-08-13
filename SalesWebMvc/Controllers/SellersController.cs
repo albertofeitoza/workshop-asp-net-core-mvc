d2b2819cbc97aa5d01324e0126054802dfefcb32
Alberto Feitoza <alberto.feitoza@outlook.com>
2019-08-13 07:33:07 -0300

    Formulário de Cadastro Vendedor

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -20,6 +21,36 @@ namespace SalesWebMvc.Controllers
         {
             var list = _SellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _SellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
         }
     }
 }
