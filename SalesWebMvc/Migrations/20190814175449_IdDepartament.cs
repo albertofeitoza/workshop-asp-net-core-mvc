b8007a82b32245fd5e95d8c4ddf039ac76f1a669
Alberto de Freitas Feitoza <alberto.feitoza@pixeon.com>
2019-08-14 15:30:14 -0300

    Complementendo Forenkey

---
+++ SalesWebMvc/Migrations/20190814175449_IdDepartament.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class IdDepartament : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
+ 
